5950dc897b427d965b381aed1eb5b47ca2f01532
mowie <mowie2@hotmail.com>
2017-11-09 00:34:58 +0100

    -Bullets move towards target -Bullets can register hits -Turrets rotate towards target -Turrets Spawn bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public GameObject Target { get; set; }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtTarget();
+        Movement();
+    }    
+    public void Movement()
+    {
+        if (Target == null)
+        {
+            Destroy(gameObject);
+            Debug.Log("Boom I got Destroyed");
+        }
+        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, Time.deltaTime * 2);
+    }
+    public void LookAtTarget()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+        Vector3 dir = Target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D hitObject)
+    {
+        Debug.Log(hitObject.tag);
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject Target { get; set; }
+    private bool canShoot { get; set; }
+    public int rateOfFire { get; set; }
+    // Use this for initialization
+    void Start()
+    {
+        LocateTarget();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtTarget();
+    }
+
+    public void LocateTarget()
+    {
+        if (Target == null)
+        {
+            CancelInvoke("ShootAtTarget");
+            canShoot = true;
+            Target = GameObject.FindGameObjectWithTag("Enemy");
+        }
+
+        
+        ShootAtTarget();
+        canShoot = false;
+
+    }
+
+    public void LookAtTarget()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+        Vector3 dir = Target.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    }
+
+    public void ShootAtTarget()
+    {
+        if (canShoot)
+        {
+            InvokeRepeating("CreateBullet", 1f, 1f);
+        }
+        
+    }
+
+    public void CreateBullet()
+    {
+        GameObject BulletObject = new GameObject();
+        BulletObject.AddComponent<Bullet>().Target = Target;
+        BulletObject.AddComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Bullet");
+        BulletObject.AddComponent<BoxCollider2D>().isTrigger = true;
+        BulletObject.transform.position = transform.position;
+    }
+}
